c63162acb885ff713b76bdcb6ef7e6a13c4b441b
athena lee <hoyan.la@gmail.com>
2024-01-18 04:42:55 +0800

    Added some pseudo codes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,20 @@
 function searchCocktails() {
     // Add functionality to search for cocktails
-    // API for cocktail data
+    // API for cocktail data (by name or ingredient)
 }
 
-// Add JavaScript code for language translation using the respective API
+// Function to store searched terms to local storage
+
+// Function to display searched terms at the side bar
+
+// API fetch request to cocktail data base
+
+// Fuction to display top 10 popular recipes if 'popular recipes' button is clicked
+
+// Function to display a random recipe if 'random recipe' button is clicked
+
+// API fetch request to translator data base
+
+//display best matched search result
+
+// Function to display translated text
