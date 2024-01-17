db5698a098eaf2f96d387468ad2705ec1623b1c3
Beata Konya <beatakonya@gmail.com>
2024-01-17 20:19:16 +0000

    created basic html,css,js

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+function searchCocktails() {
+    // Add functionality to search for cocktails
+    // API for cocktail data
+}
+
+// Add JavaScript code for language translation using the respective API
